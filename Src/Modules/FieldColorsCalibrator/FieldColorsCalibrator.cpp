8893d786f7cc3579766ec673b9b01d42944b4a3a
Francesco <petri.1797147@studenti.uniroma1.it>
2021-12-09 15:59:16 +0100

    Add initialization and initial data structures
    Implemented genome class, population array, and initialization command.
    Also some parameters in a cfg, but I expect to need more.
    
    Found some concurrency issues that most likely should be taken care of.

--- Src/Modules/FieldColorsCalibrator/FieldColorsCalibrator.cpp
+++ Src/Modules/FieldColorsCalibrator/FieldColorsCalibrator.cpp
@@ -4,19 +4,64 @@
  * @author Francesco Petri
  */
 
+//disabling warnings while importing so I don't see irrelevant messages when compiling
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#pragma GCC diagnostic ignored "-Wint-in-bool-context"
+#pragma GCC diagnostic ignored "-Wimplicit-int-float-conversion"
+#pragma GCC diagnostic ignored "-Wreorder-ctor"
+#pragma GCC diagnostic ignored "-Wmisleading-indentation"
+
 #include "FieldColorsCalibrator.h"
 #include "Tools/Debugging/Debugging.h"
 #include "Platform/SystemCall.h"
 #include "Tools/Module/Module.h"
 #include <iostream>
+#include <stdio.h>
+#include <time.h>
+
+//see above
+#pragma GCC diagnostic pop
 
 MAKE_MODULE(FieldColorsCalibrator, infrastructure)
 
-bool breakCalibration = false;
 bool loaded = false;
+bool breakCalibration = false;
+bool calibrating = false;
 
+std::vector<Genome> population;
+
+void FieldColorsCalibrator::initCalibration() {
+  OUTPUT_TEXT("Beginning color calibration");
+  SystemCall::say("Beginning color calibration");
+  std::cout << "Beginning color calibration" << std::endl;
+
+  // initialize population at random
+  population.clear();
+  srand((unsigned) time(NULL));
+  for (unsigned i=0; i<POPULATION_SIZE; i++) {
+    population.emplace_back(Genome::random());
+  }
+
+  // if the Upper and Lower threads are ACTUAL threads, then I should have serious concurrency problems here,
+  // and end up w/ a size that's neither POPULATION_SIZE nor 2*POPULATION_SIZE.
+  std::cout << population.size() << " little genomes, first: " << population[0].color_delimiter << std::endl;
+  // And in fact we DO get concurrency issues! But only if also calibrationStep does something!
+
+  // notify the update loop that calibration has begun
+  calibrating = true;
+}
+
+void FieldColorsCalibrator::calibrationStep() {
+  // testing if the population array is shared b/w threads or not
+  std::cout << "And the first is: " << population[0].color_delimiter << std::endl;
+}
+
+/**
+ * Update cycle, called at each timestep by the rest of the framework.
+ */
 void FieldColorsCalibrator::update(FieldColors& fc) {
-  // start with the initial guess provided in the cfg (for now, at least)
+  // Load the (previous) calibration stored in the configuration file, for back-compatibility in case we don't want to calibrate at all
   if (!loaded) {
     loadModuleParameters(fc, "FieldColors", nullptr);
     loaded=true;
@@ -30,7 +75,7 @@ void FieldColorsCalibrator::update(FieldColors& fc) {
     std::cout << "Hello world" << std::endl;    // prints to terminal if robot is simulated, or to log file if robot is real
   }
 
-  // a test to make sure of how I can edit the caibration parameters
+  // a test to make sure of how I can edit the caibration parameters.
   // the robot should be (and is) unable to see the ball or anything else after this command.
   DEBUG_RESPONSE_ONCE("module:FieldColorsCalibrator:breakCalibration")
   {
@@ -44,5 +89,23 @@ void FieldColorsCalibrator::update(FieldColors& fc) {
     fc.fieldHue.max = 255;
     OUTPUT_TEXT("NAO is affected by Blindness!");
     breakCalibration = false;
+  }
+
+  /**
+   * Defines a "Debug Response" for the specified command:
+   * if a "Debug Request" containing that command is issued by the simulator,
+   * the following event will be fired.
+   * (Note that the simulator can be used not only to run a simulation,
+   *  but also to connect to a real robot in order to monitor its status
+   *  and send commands such as this.)
+   */
+  DEBUG_RESPONSE_ONCE("module:FieldColorsCalibrator:startCalibration")
+  {
+    initCalibration();
+  }
+
+  // Calibrate
+  if (calibrating) {
+    calibrationStep();
   }
 }
--- Src/Modules/FieldColorsCalibrator/FieldColorsCalibrator.h
+++ Src/Modules/FieldColorsCalibrator/FieldColorsCalibrator.h
@@ -8,13 +8,55 @@
 
 #include "Representations/Configuration/FieldColors.h"
 #include "Tools/Module/Module.h"
+#include <stdio.h>
 
 MODULE(FieldColorsCalibrator,
 {,
   PROVIDES(FieldColors),
+
+  LOADS_PARAMETERS(
+  {,
+    (unsigned) MAX_GENERATIONS,   /** Maximum number of generations befire forcefully stopping the search */
+    (unsigned) POPULATION_SIZE,   /** Number of genomes in each generation */
+    (float) CROSSOVER_CHANCE,     /** Probability of crossover for each parent pair */
+    (float) MUTATION_CHANCE,      /** Probability of mutation for each child */
+    (float) MUTATION_SIGMA,       /** Standard deviation of the Gaussian mutation of each gene */
+  }),
 });
 
 class FieldColorsCalibrator : public FieldColorsCalibratorBase
 {
   void update(FieldColors& fc) override;
+  void initCalibration();
+  void calibrationStep();
+};
+
+class Genome
+{
+  public: // this class acts as little more than a struct with some constructors, so it's not going to bother with compartimentalization and other OOP principles
+
+  // arbitrary constructor
+  Genome(unsigned short cdel, unsigned short fmin, unsigned short fmax, unsigned short bwdl) {
+    color_delimiter = cdel;
+    field_min = fmin;
+    field_max = fmax;
+    black_white_delimiter = bwdl;
+  }
+
+  // random constructor. Does NOT initialize srand, that will be a responsibility of the caller (i.e. the .cpp file)
+  Genome() {
+    color_delimiter = rand() % 256;
+    field_min = rand() % 256;
+    field_max = rand() % 256;
+    black_white_delimiter = rand() % 256;
+  }
+  // alias
+  static Genome random() {
+    return Genome();
+  }
+
+  unsigned short color_delimiter;
+  unsigned short field_min;
+  unsigned short field_max;
+  unsigned short black_white_delimiter;
 };
